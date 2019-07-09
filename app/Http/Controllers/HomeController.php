7af7f9c3b100dd93e42414e4d09d695fab08fe63
香田 俊太朗 <31662724+shuntkd@users.noreply.github.com>
2019-07-09 22:27:54 +0900

    修正完了

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -30,13 +30,9 @@ class HomeController extends Controller
     {
         $randomLikes = Like::inRandomOrder()->select('guruid')->take(4)->get(); 
         $client = new Client();
-        $result = $client->request('GET', 'https://api.gnavi.co.jp/RestSearchAPI/v3/',[
-            'query' => [
-                'keyid' => '2fec3133982fe68d86c709c6594f120c',
-                'id'=> $randomLikes[0]['guruid'].','.$randomLikes[1]['guruid'].','.$randomLikes[2]['guruid'].','.$randomLikes[3]['guruid'],
-            ],
+        $result = $client->request('GET', 'https://api.gnavi.co.jp/RestSearchAPI/v3/?keyid=2fec3133982fe68d86c709c6594f120c&id='.$randomLikes[0]['guruid'].','.$randomLikes[1]['guruid'].','.$randomLikes[2]['guruid'].','.$randomLikes[3]['guruid'],
             ['http_errors' => false]
-        ]);
+        );
         $json_result=$result->getBody(); 
         $array_likes = json_decode($json_result,true);
         return view('home',['array_likes' => $array_likes]);
---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.master')
+
+@section('js')
+
+
+
+@endsection('js')
+
+@section('title',"お店をさがす")
+
+@section('content')
+
+<h3>すみません！お探しの地域でデータがありませんでした。</h3>
+<p>新しくデータを追加できる機能も実装予定ですので、今しばらくお待ちください。</p>
+
+@endsection('content')
